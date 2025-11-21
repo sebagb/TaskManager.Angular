fb96440720ccb9d17e1401f32190ad937d56ee13
sebagb <sgbcmpu@gmail.com>
2025-11-21 22:44:27 +0100

    Add project-preview component

---
+++ src/app/project-preview/project-preview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-preview',
+  imports: [],
+  templateUrl: './project-preview.html',
+  styleUrl: './project-preview.css',
+})
+export class ProjectPreview {
+
+}
