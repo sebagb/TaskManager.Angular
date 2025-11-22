75bccf59fadeb14726e50ace3d8e2d166e559215
sebagb <sgbcmpu@gmail.com>
2025-11-22 16:41:03 +0100

    Implement project filtering

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -9,25 +9,42 @@ import { ProjectPreview } from '../project-preview/project-preview';
   styleUrl: './projects.css',
 })
 export class Projects {
-  projectsList: ProjectInfo[] = [{
-    id: 111111,
-    deadline: 1201,
-    status: 'Pending',
-    tasks: '',
-    title: 'Love',
-  },
-  {
-    id: 222222,
-    deadline: 1201,
-    status: 'Complete',
-    tasks: '',
-    title: 'Job',
-  },
-  {
-    id: 333333,
-    deadline: 1201,
-    status: 'Halt',
-    tasks: '',
-    title: 'Housing',
-  }];
+  projectsList: ProjectInfo[] = [];
+  filteredProjectsList: ProjectInfo[] = [];
+
+  constructor() {
+    this.projectsList = [{
+      id: 111111,
+      deadline: 1201,
+      status: 'Pending',
+      tasks: '',
+      title: 'Love',
+    },
+    {
+      id: 222222,
+      deadline: 1201,
+      status: 'Complete',
+      tasks: '',
+      title: 'Job',
+    },
+    {
+      id: 333333,
+      deadline: 1201,
+      status: 'Halt',
+      tasks: '',
+      title: 'Housing',
+    }];
+
+    this.filteredProjectsList = this.projectsList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredProjectsList = this.projectsList;
+      return;
+    }
+
+    this.filteredProjectsList = this.projectsList.filter(project =>
+      project?.title.toLowerCase().includes(text.toLowerCase()),);
+  }
 }
