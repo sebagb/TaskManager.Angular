ce5de2ed20f4ef6218af4f3e64a69fbe8344257e
sebagb <sgbcmpu@gmail.com>
2025-11-22 16:50:37 +0100

    Add project-detail component

---
+++ src/app/project-detail/project-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-detail',
+  imports: [],
+  templateUrl: './project-detail.html',
+  styleUrl: './project-detail.css',
+})
+export class ProjectDetail {
+
+}
