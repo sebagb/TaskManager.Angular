9077e00ceeb875f03afcff86b00e370918a8ffcf
sebagb <sgbcmpu@gmail.com>
2025-11-26 09:13:26 +0100

    Generate user-access component and interface

---
+++ src/app/user-access/user-access.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-access',
+  imports: [],
+  templateUrl: './user-access.html',
+  styleUrl: './user-access.css',
+})
+export class UserAccess {
+
+}
