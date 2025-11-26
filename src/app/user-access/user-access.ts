a61ac3aab235f92c16658cbf9fe909289aa1a51a
sebagb <sgbcmpu@gmail.com>
2025-11-26 12:01:26 +0100

    Implement UserService register

--- src/app/user-access/user-access.ts
+++ src/app/user-access/user-access.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../user-service';
 
 @Component({
   selector: 'app-user-access',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './user-access.css',
 })
 export class UserAccess {
+  private service = inject(UserService);
+  public message: string | undefined;
 
+  public register(username: string, password: string) {
+    this.service.register(username, password)
+      .then(response => {
+        this.message = response.status.toString();
+      });
+  }
+
+  public login(username: string, password: string) {
+
+  }
 }
--- src/app/user-service.ts
+++ src/app/user-service.ts
@@ -4,5 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-  
+  private readonly base = 'http://localhost:8080/member';
+
+  public async register(username: string, password: string): Promise<Response> {
+    const url = `${this.base}/register`
+    const request = new Request(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password,
+      }),
+    });
+    const data = await fetch(request);
+    return data;
+  }
 }
